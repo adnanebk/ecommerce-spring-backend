cbaa0d89b90e013b94a09bc07818949bf3601fcb
adnanebk <benkouider.adnane@gmail.com>
2022-02-05 01:42:48 +0200

    remove unused

--- src/main/java/com/adnanbk/ecommerce/reposetories/CreditCardRepo.java
+++ src/main/java/com/adnanbk/ecommerce/reposetories/CreditCardRepo.java
@@ -1,7 +1,6 @@
 package com.adnanbk.ecommerce.reposetories;
 
 import com.adnanbk.ecommerce.models.CreditCard;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
