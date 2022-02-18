a51f30238f5f13df36351a81870a905f0c96879a
adnanebk <benkouider.adnane@gmail.com>
2022-02-18 22:47:13 +0100

    fixe

--- src/main/java/com/adnanbk/ecommerce/controllers/AuthController.java
+++ src/main/java/com/adnanbk/ecommerce/controllers/AuthController.java
@@ -11,6 +11,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,7 +20,7 @@ import java.net.URI;
 import java.security.Principal;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping(value = "/api", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
 public class AuthController {
 
