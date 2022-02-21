3d1a26c77cca81c6c33ddf4b8284893f971a3870
adnane <31042158+adnanebk@users.noreply.github.com>
2022-02-21 21:49:48 +0100

    test

--- src/main/java/com/adnanbk/ecommerce/aspects/ProductAspect.java
+++ src/main/java/com/adnanbk/ecommerce/aspects/ProductAspect.java
@@ -10,8 +10,7 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 import java.util.Optional;
 
-@Aspect
-@Component
+
 public class ProductAspect {
 
     @Value("${imagesPathUrl}")
