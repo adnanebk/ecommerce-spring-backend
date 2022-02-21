072e4a64f3db48f2195025e87f1d10e263f91213
adnanebk <benkouider.adnane@gmail.com>
2022-02-21 13:22:24 +0100

    fixe

--- src/main/java/com/adnanbk/ecommerce/aspects/ProductAspect.java
+++ src/main/java/com/adnanbk/ecommerce/aspects/ProductAspect.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 
 @Aspect
 @Component
@@ -17,16 +18,16 @@ public class ProductAspect {
     private String imagesPathUrl;
 
 
-
+    @AfterReturning(value = "execution(* com.adnanbk.ecommerce.reposetories.ProductRepository.findById(..))",returning = "product")
+    public void findByAspect(Optional<Product>  product) {
+        product.ifPresent(prod->prod.setImage(imagesPathUrl+prod.getImage()));
+    }
     @AfterReturning(value = "execution(* com.adnanbk.ecommerce.reposetories.ProductRepository.find*(..,org.springframework.data.domain.Pageable))",returning = "products")
     public void findAllAspect(Page<Product>  products) {
 
         products.forEach(product -> product.setImage(imagesPathUrl+product.getImage()));
     }
-    @AfterReturning(value = "execution(* com.adnanbk.ecommerce.reposetories.ProductRepository.findBy*(..))",returning = "product")
-    public void findByAspect(Product  product) {
-        product.setImage(imagesPathUrl+product.getImage());
-    }
+
     @AfterReturning(value = "execution(* com.adnanbk.ecommerce.reposetories.ProductRepository.saveAll*(..))",returning = "products")
     public void saveAllAllAspect(List<Product> products) {
         products.forEach(product -> product.setImage(imagesPathUrl+product.getImage()));
