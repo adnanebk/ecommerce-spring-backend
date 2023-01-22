ce40fe47c764409cc87daa9f21eccfceabfcd0e0
abenkoui <benkouider.adnane@capgemini.com>
2023-01-23 00:41:46 +0100

    remove unused import

--- src/main/java/com/adnanbk/ecommerce/dto/PageDto.java
+++ src/main/java/com/adnanbk/ecommerce/dto/PageDto.java
@@ -2,9 +2,6 @@ package com.adnanbk.ecommerce.dto;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 
 @Getter
 @Setter
