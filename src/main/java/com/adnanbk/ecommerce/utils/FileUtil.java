e0483020009af47bf63fae792556ed460fe57518
abenkoui <benkouider.adnane@capgemini.com>
2023-02-06 12:27:06 +0100

    refactor

--- src/main/java/com/adnanbk/ecommerce/utils/FileUtil.java
+++ src/main/java/com/adnanbk/ecommerce/utils/FileUtil.java
@@ -2,11 +2,8 @@ package com.adnanbk.ecommerce.utils;
 
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
-
-import javax.annotation.PostConstruct;
 
 @Component
 public class FileUtil {
@@ -16,15 +13,15 @@ public class FileUtil {
 
 
     @Value("${uploads.imagesPathUrl}")
-    private void setImagesPathUrl(String imagesPathUrl){
+    private synchronized void setImagesPathUrl(String imagesPathUrl){
         FileUtil.imagesPathUrl=imagesPathUrl;
     }
     @Value("${uploads.externalImagesPathUrl}")
-    private void setExternalImagesPathUrl(String externalImagesPathUrl){
+    private synchronized void  setExternalImagesPathUrl(String externalImagesPathUrl){
         FileUtil.externalImagesPathUrl=externalImagesPathUrl;
     }
 
-    public static String toImageUrl(String imageName) {
+    public  static String toImageUrl(String imageName) {
         String url = "";
         if(StringUtils.hasLength(imageName) && !imageName.startsWith("http"))
             url=imageName.contains("luv2code") ?externalImagesPathUrl:imagesPathUrl;
