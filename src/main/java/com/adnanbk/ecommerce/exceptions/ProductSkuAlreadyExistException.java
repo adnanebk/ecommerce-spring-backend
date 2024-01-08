1e57784efcb39972e1d6c840b1c5569822edbb3d
adnane <adnane.benkouider@capgemini.com>
2024-01-08 13:14:59 +0100

    add missing exception

---
+++ src/main/java/com/adnanbk/ecommerce/exceptions/ProductSkuAlreadyExistException.java
@@ -0,0 +1,13 @@
+package com.adnanbk.ecommerce.exceptions;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class ProductSkuAlreadyExistException  extends RuntimeException {
+
+    public ProductSkuAlreadyExistException(String message) {
+        super(message);
+    }
+}
